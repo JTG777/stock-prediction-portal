f281aab458fa5ef69feaa1281127198552354b87
Joseph T George <120744239+JTG777@users.noreply.github.com>
2025-12-11 16:11:04 +0530

    homepage

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='py-3 my-3 footer'>
+        <hr className="border-bottom" />
+        <p className='text-light text-center'>&copy; 2025 Build for Learning</p>
+
+    </footer>
+    
+    </>
+)
+}
+
+export default Footer
